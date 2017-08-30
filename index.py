6dfaab020cf157d9f2eebde67b23c6cb453be889
Anderson Cordeiro <andersoncordeironf@gmail.com>
2017-08-30 14:51:03 -0300

    change message

--- index.py
+++ index.py
@@ -14,14 +14,14 @@ def webhook():
             data = json.loads(request.data.decode())
             text = data['entry'][0]['messaging'][0]['message']['text']
             sender = data['entry'][0]['messaging'][0]['sender']['id']
-            payload = {'recipient': {'id': sender}, 'message': {'text': "olá, tudo bem? como vai?"}}
+            payload = {'recipient': {'id': sender}, 'message': {'text': "olá, tudo bem?"}}
             r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
         except Exception as e:
             print(traceback.format_exc())
     elif request.method == 'GET': # Para a verificação inicial
         if request.args.get('hub.verify_token') == os.environ.get('FB_VERIFY_TOKEN'):
             return request.args.get('hub.challenge')
-        return "Hi, i am Getulio!"
+        return "Hi, i am a robot!"
     return "Nothing"
 
 
